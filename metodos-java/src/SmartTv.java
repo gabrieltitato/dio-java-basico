9c42115ced983395d141f849e20d43f8a1abaf8c
gennun <titatoou2@hotmail.com>
2024-06-05 15:45:56 -0300

    aprendendo metodos com java

---
+++ metodos-java/src/SmartTv.java
@@ -0,0 +1,31 @@
+public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 15;
+
+    public void ligar(){
+        ligada = true;
+    }
+    
+    public void desligar(){
+        ligada = false;
+    }
+    
+    public void aumentarVolume(){
+        volume++;
+    }
+
+    public void diminuirVolume(){
+        volume--;
+    }
+
+    public void proximoCanal(){
+        canal++;
+    }
+
+    public void anteriorCanal(){
+        canal--;
+    }
+
+}
+
---
+++ metodos-java/src/Usuario.java
@@ -0,0 +1,20 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+
+        smartTv.proximoCanal();
+
+        System.out.println("TV Ligada? " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println(smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println(smartTv.ligada);
+    }
+}
