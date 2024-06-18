8befd16b6fbcae89387ce5fa54ed9bb7426ff3df
gennun <titatoou2@hotmail.com>
2024-06-18 18:52:54 -0300

    Condicionais

---
+++ controle-fluxo/controle-fluxo/src/PlanoOperadora.java
@@ -0,0 +1,18 @@
+public class PlanoOperadora {
+    public static void main(String[] args) throws Exception {
+        String plano = "M"; //M / T / B
+
+        switch (plano) {
+            case "T":
+                System.out.println("5GB Youtube");
+                
+        
+            case "M":
+                System.out.println("WhatsApp e Instagram");
+                
+            
+            case "B":
+                System.out.println("100 minutos de chamadas");
+            }   
+        }
+}
